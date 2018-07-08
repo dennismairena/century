9ee8d15dc21230ed01de4f7585d90bfc268ce73a
Dennis Mairena <dmg19@hotmail.com>
2018-07-07 20:14:19 -0600

    test
    test

--- js/arriba.js
+++ js/arriba.js
@@ -1,15 +1,27 @@
 jQuery.noConflict();
 jQuery(document).ready(function() {
 jQuery("#IrArriba").hide();
+
+
 jQuery(function () {
 jQuery(window).scroll(function () {
 if ($(this).scrollTop() > 10) {
+
 jQuery('#IrArriba').fadeIn('slow');
+
 } 
+
 else {
+  
+
+	
+	
 jQuery('#IrArriba').fadeOut('slow');
+
+
 }
 });
+
 jQuery('#IrArriba a').click(function () {
 jQuery('body,html').animate({
 scrollTop: 0
@@ -22,5 +34,9 @@ scrollTop: 350
 }, 800);
 return false;
 });
+
+
+
 });
+
 });
